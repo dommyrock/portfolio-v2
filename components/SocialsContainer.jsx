9c4d637a848785a4050af9434b6cd770375e7ee4
dominik-polzer <dominik.polzer-fve@bullhorn.com>
2022-03-27 14:28:02 +0200

    cv section added

--- components/SocialsContainer.jsx
+++ components/SocialsContainer.jsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 const SocialsContainer = () => {
   return (
     <div className="flex items-center w-full mt-32">
-      <div style={{borderRadius: '999px', overflow: 'hidden',display:'flex'}}>
+      <div
+        style={{ borderRadius: "999px", overflow: "hidden", display: "flex" }}
+      >
         <Image
           src="/ppict.png"
           alt="profile-picture"
-          layout="intrinsic" 
+          layout="intrinsic"
           width={64}
           height={64}
         />
@@ -48,6 +50,28 @@ const SocialsContainer = () => {
               <path d="M24 4.557a9.83 9.83 0 01-2.828.775 4.932 4.932 0 002.165-2.724 9.864 9.864 0 01-3.127 1.195 4.916 4.916 0 00-3.594-1.555c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 3.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 19.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 4.557z"></path>
             </svg>
             <span>Twitter</span>
+          </a>
+        </li>
+        <li className="pt-4">
+          <a
+            href="https://docs.google.com/document/d/10Dv7QXqrctqUafpkycXAXnjmN4Jv1baYUXhJAenVOsY/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center"
+            id="cv-section"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6 mr-6 fill-current"
+              fill="none" viewBox="0 0 24 24" stroke="black " strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+              />
+            </svg>
+            <span>CV</span>
           </a>
         </li>
       </ul>
